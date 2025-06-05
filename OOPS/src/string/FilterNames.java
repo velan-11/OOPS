93956ae276567858cb19a7f02653e9b37001537d
velan-11 <chick11112004@gmail.com>
2025-06-05 13:29:04 +0530

    intial commit

---
+++ OOPS/src/string/FilterNames.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.util.Scanner;
+
+public class FilterNames {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int size=sc.nextInt();
+		String s[]=new String[size];
+		for(int i=0;i<size;i++) {
+			s[i]=sc.next();
+		}
+		char ch=sc.next().charAt(0);
+		filter(s,ch);
+	}
+	public static void filter(String a[],char ch) {
+	
+		for(int i=0;i<a.length;i++) {
+			if(a[i].toLowerCase().charAt(0)==ch) {
+				System.out.println(a[i]);
+				
+			}
+			
+		}
+	}
+}
